a73dbebd80cf18d9938980ec12c2e3da758f86e1
Mdryn <mdryndin@gmail.com>
2019-11-02 22:21:48 +0200

    CustomUserCollection

---
+++ Homework_2_01/Homework_2_01/CustomUserCollection.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace UserCollection
+{
+    // Коллекция объектов типа string. Этот класс
+    // реализует IEnumerable, чтобы его можно было использовать
+    // с синтаксисом ForEach.
+    class UserCollection<T> : IEnumerable
+    {
+        public List<T> ListCollection;
+
+        public void Add(T value)
+        {
+            ListCollection.Add(value);
+        }
+        public void Remove()
+        {
+            if(ListCollection.Count > 5)
+            {
+                ListCollection.RemoveAt(ListCollection.Count-1);
+            }
+            
+        }
+        // Реализация для метода GetEnumerator.
+        public IEnumerator GetEnumerator()
+        {
+            return new Iterator<T>(this);
+        }
+    }
+
+    class Iterator<T> : IEnumerator
+    {
+        List<T> myListCollection;
+        public Iterator(UserCollection<T> col)
+        {
+            myListCollection = col.ListCollection;
+        }
+
+        // Перечислители располагаются перед первым элементом
+        // до первого вызова MoveNext ().
+        int currentPosition = -1;
+
+        public object Current
+        {
+            get
+            {
+                return myListCollection[currentPosition];
+            }
+        }
+
+        public bool MoveNext()
+        {
+            if (currentPosition < myListCollection.Count - 1)
+            {
+                currentPosition++;
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Reset()
+        {
+            currentPosition = -1;
+        }
+
+    }
+}
---
+++ Homework_2_01/Homework_2_01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace UserCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserCollection<string> collection = new UserCollection<string>();
+            collection.ListCollection = new List<string>() { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" };
+            collection.Add("Saturday");
+            Console.ReadKey();
+            collection.Remove();
+
+            foreach (var element in collection)
+                Console.WriteLine(element);
+            Console.ReadKey();
+            
+            UserCollection<int> collection2 = new UserCollection<int>();
+            collection2.ListCollection = new List<int>() { 1, 2, 3, 4, 5, 6, 7 };
+            collection2.Add(100);
+            collection2.Add(200);
+            collection2.Add(300);
+
+            foreach (var element in collection2)
+                Console.WriteLine(element);
+
+            Console.ReadKey();
+            
+        }
+    }
+}
