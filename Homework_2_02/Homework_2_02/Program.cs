6ebb15c371aea2f313aafac786319bf194f5ecde
Mdryn <mdryndin@gmail.com>
2019-11-03 19:40:02 +0200

    CustomGenericClass

---
+++ Homework_2_02/Homework_2_02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_2_02
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle<int, Shape> triangle = new Triangle<int, Shape>();
+
+            triangle.ShapeMethod();
+
+        }
+    }
+}
---
+++ Homework_2_02/Homework_2_02/Triangle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_2_02
+{
+  class Triangle<T,R>
+        where T : struct
+        where R : Shape
+    {
+        Shape shape;
+        private T id;
+
+        public (T, Shape) ShapeMethod()  
+        {
+            return (id, shape);
+        }
+
+    }
+}
