867eb06a4135401593364e18c71233246291b49b
Mdryn <mdryndin@gmail.com>
2019-11-03 18:16:05 +0200

    BigBadWolf

---
+++ Homework_Adv_3_01/Homework_Adv_3_01/BigBadWolf.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_Adv_3_01
+{
+   public class BigBadWolf
+    {
+
+        public string name;
+
+        public BigBadWolf(string name)
+        {
+            this.name = name;
+        }
+        /*
+        public string GetName(string name)
+        {
+            return $"Волк: {name}";
+        }
+        */
+    }
+}
---
+++ Homework_Adv_3_01/Homework_Adv_3_01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace Homework_Adv_3_01
+{
+    class Program
+    {
+
+        public delegate BigBadWolf WolfDelegate(string name);
+
+        static void Main(string[] args)
+        { 
+            BigBadWolf bigBadWolf = new BigBadWolf("GreyWolf");
+
+                WolfDelegate wolfDelegate;
+
+                wolfDelegate = BigBadWolf => bigBadWolf.name;
+
+             Console.WriteLine(wolfDelegate.Invoke(name: "Wolf"));
+
+            Console.ReadKey();
+            
+        
+
+        }
+       
+    }
+}
