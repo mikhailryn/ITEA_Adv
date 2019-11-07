a311ecb2cf98c4935f0c3a64976700c4deb808c0
Mdryn <mdryndin@gmail.com>
2019-11-07 18:05:15 +0200

    Correct

---
+++ Homework_1_01_Correct/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_01
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<IKey,IValue> correct = new Dictionary<IKey,IValue>();
+            correct.Add(new Key(), new Value());
+            correct.Add(new Key(), new Value());
+            correct.Add(new Key(), new Value());
+        }
+  
+    }
+
+  
+}
