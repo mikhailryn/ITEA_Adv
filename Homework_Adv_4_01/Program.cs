6db1eff6d562ac6ae793b9f5da17f47e9ea22e99
Mdryn <mdryndin@gmail.com>
2019-11-08 19:09:56 +0200

    Delegate lambda

---
+++ Homework_Adv_4_01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_4
+{
+    class Program
+    {
+        public delegate T GenericDelegate<T>(T obj1);
+        static string NameMethod(string name)
+        {
+            return $"First name: {name}";
+        }
+        static void Main(string[] args)
+        {
+            GenericDelegate<string> genericDelegate = NameMethod;
+
+            genericDelegate = name => NameMethod("Petro");
+
+            Console.WriteLine(genericDelegate(""));
+            Console.ReadKey();
+        }
+    }
+}
