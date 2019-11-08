8d3a3ac68a5a6f67930bd48169d741ca8277fd79
Mdryn <mdryndin@gmail.com>
2019-11-08 19:11:26 +0200

    Syntax simplification delegate

---
+++ Homework_Adv_4_02/Homework_Adv_4_02/Call.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_Adv_4_02
+{
+    class Call
+    {
+        public event Action PhoneCall;
+        public void DoEvent()
+        {
+            PhoneCall?.Invoke();
+        }
+    }
+}
---
+++ Homework_Adv_4_02/Homework_Adv_4_02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Homework_Adv_4_02
+{
+    class Program
+    {
+        public delegate void MyDelegate();
+        public event MyDelegate PhoneCall;
+        static void Main(string[] args)
+        {
+            Call call = new Call();
+
+            call.PhoneCall += () => Console.WriteLine($"Звонок от");
+
+            call.DoEvent();
+
+            Console.ReadKey();
+        }   
+    }
+}
