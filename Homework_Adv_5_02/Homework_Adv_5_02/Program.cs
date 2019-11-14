92e91cd4411dc3d4475631597bd462ebaefb051a
Mdryn <mdryndin@gmail.com>
2019-11-14 21:25:45 +0200

    Task 2

---
+++ Homework_Adv_5_02/Homework_Adv_5_02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_Adv_5_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StaticClass.CallDelegate(() => Console.WriteLine("А так можно было?"));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Homework_Adv_5_02/Homework_Adv_5_02/StaticClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_Adv_5_02
+{
+    public delegate void MyDelegate();
+    static class StaticClass
+    {
+        public static void CallDelegate(MyDelegate myDelegate)
+        {
+            myDelegate();
+        }
+    }
+}
