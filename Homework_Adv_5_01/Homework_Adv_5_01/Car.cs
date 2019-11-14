07da7736357f96cd8f39a8628ac097203dc7cade
Mdryn <mdryndin@gmail.com>
2019-11-14 21:22:36 +0200

    Task 1

---
+++ Homework_Adv_5_01/Homework_Adv_5_01/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_Adv_5_01
+{
+    class Car
+    {
+        public static void OnEngine(Action actionOnEngine)
+        {
+            actionOnEngine();
+        }
+    }
+}
---
+++ Homework_Adv_5_01/Homework_Adv_5_01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_Adv_5_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Action action = SomePhrase;
+
+           Car.OnEngine(action);
+
+           void SomePhrase()
+            {
+                Console.WriteLine("Start the engine");
+            }
+            
+        }
+    }
+}
