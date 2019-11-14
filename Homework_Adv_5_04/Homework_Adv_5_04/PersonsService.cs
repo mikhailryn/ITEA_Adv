de86b746b7ad5a6571104b9e0ffa8f9211ec47b6
Mdryn <mdryndin@gmail.com>
2019-11-14 21:28:06 +0200

    Task 4

---
+++ Homework_Adv_5_04/Homework_Adv_5_04/PersonsService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_Adv_5_04
+{
+    public static class PersonsService
+    {
+        public static List<Person> GetPersons()
+        {
+            var persons = new List<Person>();
+
+            persons.Add(new Person("Finn", "Mertens", true, 14));
+            persons.Add(new Person("Philip", "Fry", true, 25));
+            persons.Add(new Person("Arven", "Menethil", false, 2700));
+            persons.Add(new Person("Audrey", "Hepburn", false, 29));
+            persons.Add(new Person("John", "Wick", true, 42));
+            persons.Add(new Person("Anakin", "Skywalker", true, 29));
+            persons.Add(new Person("Princess", "Bubblegum", false, 827));
+            persons.Add(new Person("Svetlana", "Sokolova", false, 45));
+            persons.Add(new Person("Varian", "Wrynn", true, 42));
+
+            return persons;
+        }
+    }
+}
---
+++ Homework_Adv_5_04/Homework_Adv_5_04/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_Adv_5_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var characters = PersonsService.GetPersons();
+
+            // Сортировка в порядке убывания по возрасту
+            var sortedAge = characters.Where(x => x.Age > 30).OrderByDescending(x => x.Age);  
+
+            foreach(var adult in sortedAge)
+                Console.WriteLine(adult.ToString());
+            Console.WriteLine();
+
+            // Сортировка по возрастанию FirstName
+            var sortedName = characters.Where(x => x.Age > 30).OrderBy(x => x.FirstName);
+
+            foreach (var name in sortedName)
+                Console.WriteLine(name.ToString());
+            Console.ReadKey();
+        }
+    }
+}
