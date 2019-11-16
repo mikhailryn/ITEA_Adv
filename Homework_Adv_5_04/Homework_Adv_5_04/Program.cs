018575b29062cb62fea85dbdb22001d7f0b87f5a
Mdryn <mdryndin@gmail.com>
2019-11-16 16:48:58 +0200

    New request

--- Homework_Adv_5_04/Homework_Adv_5_04/Program.cs
+++ Homework_Adv_5_04/Homework_Adv_5_04/Program.cs
@@ -12,18 +12,11 @@ namespace Homework_Adv_5_04
         {
             var characters = PersonsService.GetPersons();
 
-            // Сортировка в порядке убывания по возрасту
-            var sortedAge = characters.Where(x => x.Age > 30).OrderByDescending(x => x.Age);  
 
-            foreach(var adult in sortedAge)
-                Console.WriteLine(adult.ToString());
-            Console.WriteLine();
+            var sortedPerson = characters.Where(x => x.Age > 30).OrderByDescending(x => x.Age).ThenBy(x => x.FirstName);  
 
-            // Сортировка по возрастанию FirstName
-            var sortedName = characters.Where(x => x.Age > 30).OrderBy(x => x.FirstName);
-
-            foreach (var name in sortedName)
-                Console.WriteLine(name.ToString());
+            foreach(var person in sortedPerson)
+                Console.WriteLine(person.ToString());
             Console.ReadKey();
         }
     }
