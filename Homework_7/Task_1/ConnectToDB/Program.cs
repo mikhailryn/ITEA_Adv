a48897f5b9077750c86fabf3a28591871f755280
mikhailryn <mdryndin@gmail.com>
2019-11-22 16:02:35 +0200

    homework 7

---
+++ Homework_7/Task_1/ConnectToDB/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ConnectToDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queryString = @"update Characters set LastNamr = 'Undomiel' where FirstName = 'Arven'";
+
+            using (SqlConnection connection = new SqlConnection())
+            {
+                connection.ConnectionString = ConfigurationManager.ConnectionStrings["TestDatabases"].ToString();
+                SqlCommand command = new SqlCommand(queryString, connection);
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ Homework_7/Task_4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Homework_Adv_7_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queryString = @"select * from Movies";
+
+            using (SqlConnection connection = new SqlConnection())
+            {
+                connection.ConnectionString = ConfigurationManager.ConnectionStrings["Homework_7_DB"].ToString();
+                SqlCommand command = new SqlCommand(queryString, connection);
+                connection.Open();
+
+                using (SqlDataReader sqlReader = command.ExecuteReader())
+                {
+                    while (sqlReader.Read())
+                    {
+                        Console.WriteLine($"Name: {sqlReader[0].ToString()}, Genre: {sqlReader[1].ToString()}," +
+                            $"  Year: {sqlReader[2].ToString()}");
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
