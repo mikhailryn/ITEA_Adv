84beb919384c9ea33422cff8ca10f819eff7b578
mikhailryn <mdryndin@gmail.com>
2019-11-22 15:56:54 +0200

    task 3

---
+++ Homework_Adv_7_03/Homework_Adv_7_02/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Homework_Adv_7_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queryString =
+               @"insert into Movies ([Name], [Genre], [Year])
+                values ('A beautiful mind', 'Biography, Drama', '2001');
+                insert into Movies ([Name], [Genre], [Year])
+                values('Брат 2', 'Боевик, триллер', '2000');
+                insert into Movies ([Name], [Genre], [Year])
+                values('The Terminal', 'Comedy, Drama', '2004');";
+                
+            using (SqlConnection connection = new SqlConnection())
+            {
+                connection.ConnectionString = ConfigurationManager.ConnectionStrings["Homework_7_DB"].ToString();
+                SqlCommand command = new SqlCommand(queryString, connection);
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
+
