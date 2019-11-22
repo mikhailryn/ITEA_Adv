9c7d103005f7f6dab5b91c643f898ea511b12e98
mikhailryn <mdryndin@gmail.com>
2019-11-22 15:50:54 +0200

    task 2

---
+++ Homework_Adv_7_02/Homework_Adv_7_02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Homework_Adv_7_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            var queryString = @"CREATE TABLE Movies" + "Name NVARCHAR(50), Genre NVARCHAR(50), Year INT";
+
+
+            using (SqlConnection connection = new SqlConnection())
+            {
+                connection.ConnectionString = ConfigurationManager.ConnectionStrings["Homework_7_DB"].ToString();
+                SqlCommand command = new SqlCommand(queryString, connection);
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
+
