8757f2c3f7031a1559f0a7149d39717421ce0e67
mikhailryn <mdryndin@gmail.com>
2019-11-23 21:46:26 +0200

    Entity Framework

---
+++ Homework_Adv_8/Homework8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PractAdv8;
+
+namespace Homework8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (BookShelfContext book = new BookShelfContext())
+            {
+                
+                var character = new Character();
+                //Добавление в БД модели персонажей
+                book.Characters.Add(character);
+
+                var bookService = new BookShelfService();
+
+                //Вызов метода заполнения таблицы персонажами
+                bookService.AddCharacterBookShelf();
+
+                //Вывод персонажей в консоль
+                bookService.GetBookShelf();
+
+                //Вставка записи в таблицу Movies
+                bookService.AddMovies();
+            }
+        }
+    }
+}
