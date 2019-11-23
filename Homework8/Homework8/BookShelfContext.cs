f58195fac1692d6795c82d9182ebc9556d31a426
mikhailryn <mdryndin@gmail.com>
2019-11-23 12:23:49 +0200

    test

---
+++ Homework8/Homework8/BookShelfContext.cs
@@ -0,0 +1,19 @@
+﻿using PractAdv8;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework8
+{
+    class BookShelfContext:DbContext
+    {
+        public BookShelfContext(): base("BookShelfContext")
+        {
+
+        }     
+        public DbSet<Character> Characters { get; set; }
+    }
+}
---
+++ Homework8/Homework8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (BookShelfContext book = new BookShelfContext())
+            {
+
+            }
+        }
+    }
+}
