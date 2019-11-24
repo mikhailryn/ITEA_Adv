9fcd41ba903b901b1ae97030cae08db0b6b75e90
mikhailryn <mdryndin@gmail.com>
2019-11-24 13:09:51 +0200

    update add method

---
+++ Homework_8/Homework_8/BookShelfContext.cs
@@ -0,0 +1,19 @@
+﻿using PractAdv8;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_8
+{
+    class BookShelfContext: DbContext
+    {
+        public BookShelfContext():base("BookShelfContext")
+        {
+
+        }
+        public DbSet<Character> Characters { get; set; }
+    }
+}
---
+++ Homework_8/Homework_8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PractAdv8;
+
+namespace Homework_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new BookShelfContext())
+            {
+
+            }
+
+        }
+    }
+}
