97ac2ef41e2ac105cf37a570a1adb115f35212e4
Mdryn <mdryndin@gmail.com>
2019-11-30 18:39:55 +0200

    Directors DB create

---
+++ Homework_Adv_9_01/Homework_Adv_9_01/AddDirectors.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.Linq;
+
+namespace Homework_Adv_9_01
+{
+   public class AddDirectors
+    {
+        public static void InsertDirectors()
+        {
+            DataContext dataContext = new DataContext(ConfigurationManager.ConnectionStrings["CinemaConnect"].ToString());
+
+            var directorOne = new Directors() { FirstName = "Quentin", LastName = "Tarantino", BirthDate = 1963 };
+            var directorTwo = new Directors() { FirstName = "Guy", LastName = "Ritchie", BirthDate = 1968 };
+            var directorThree = new Directors() { FirstName = "Алексей", LastName = "Балабанов", BirthDate = 1959 };
+
+            dataContext.GetTable<Directors>().InsertAllOnSubmit(new Directors[] { directorOne,directorTwo, directorThree});
+            
+            dataContext.SubmitChanges();
+            Console.WriteLine("Directors inserted");
+        }
+    }
+}
---
+++ Homework_Adv_9_01/Homework_Adv_9_01/Directors.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Linq.Mapping;
+
+namespace Homework_Adv_9_01
+{
+    [Table(Name = "Directors")]
+   public class Directors
+    {
+        [Column(IsPrimaryKey = true, IsDbGenerated = true)]
+        public int Id { get; set; }
+        [Column]
+        public string FirstName { get; set; }
+        [Column]
+        public string LastName { get; set; }
+        [Column]
+        public int BirthDate { get; set; }
+    }
+}
