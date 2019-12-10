e9beea1635de2e036bafd3026aae9e9b21ee59c2
Mdryn <mdryndin@gmail.com>
2019-12-10 16:15:40 +0200

    IO task

---
+++ Homework_10_01/Homework_10_01/DirectoryClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Homework_10_01
+{
+   public static class DirectoryClass
+    {
+       
+        public static void IOMethod()
+        {
+            var fileInfo = new FileInfo(@"D:\NotExistingDir1\NotExistsingDir2\NewFile.txt");
+
+            if (!fileInfo.Exists)
+            {
+                try
+                {
+                    fileInfo.Create();
+                    Console.WriteLine($"File:{fileInfo.FullName} created");
+                }
+                catch 
+                {
+                    var directory = new DirectoryInfo(@"D:\NotExistingDir1\NotExistsingDir2\");
+                    directory.Create();
+                    Console.WriteLine($"Directory {directory.FullName} was created");
+                }
+                
+               
+            }
+            
+            
+
+
+        }
+        
+    }
+}
---
+++ Homework_10_01/Homework_10_01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_10_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryClass.IOMethod();
+            Console.ReadKey();
+        }
+    }
+}
