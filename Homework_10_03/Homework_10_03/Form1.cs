b25989ae46370096f1a835bee569a8cb7411ffd9
Mdryn <mdryndin@gmail.com>
2019-12-10 16:17:40 +0200

    WinForm load picture

---
+++ Homework_10_03/Homework_10_03/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Homework_10_03
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+            //pictureBox1.ImageLocation = 
+        }
+
+        OpenFileDialog ofd = new OpenFileDialog();
+       
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            
+            ofd.Title = "Select your image";
+
+            
+
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                Bitmap image = new Bitmap(ofd.FileName);
+                pictureBox1.Image = image;
+            }
+        }
+    }
+}
